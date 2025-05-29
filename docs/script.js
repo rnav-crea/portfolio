ea2c009779c2513a76945659bcc755229e520339
rnav <reddinaveen2005@gmail.com>
2025-05-29 15:27:38 +0530

    add js to remove when selecting toggle and add css to allign it

--- docs/script.js
+++ docs/script.js
@@ -2,13 +2,21 @@ function projects() {
     console.log("Projects button clicked");
     // window.location.href = "Assets/projects.html";
 }
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.querySelector('.menu-toggle');
     const menuLinks = document.querySelector('.menu-links');
+    const menuLinkItems = document.querySelectorAll('.menu-links li a');
     if (menuToggle && menuLinks) {
-        menuToggle.addEventListener('click', function() {
+        menuToggle.addEventListener('click', function () {
             menuLinks.classList.toggle('active');
         });
     }
-});
+    menuLinkItems.forEach(link => {
+        link.addEventListener('click', function () {
+            if (menuLinks.classList.contains('active')) {
+                menuLinks.classList.remove('active');
+            }
+        });
+    })
 
+});
